0cc8bd18fd64518f4342024e147c47a2ad343613
toddLM <65785960+toddLM@users.noreply.github.com>
2021-06-28 09:20:51 -0700

    working with variables

--- assets/variables.js
+++ assets/variables.js
@@ -1,4 +1,20 @@
-console.log("hello");
+let color = "red";
+console.log(color);
+
+color = "blue"
+console.log(color);
+
+const shape = ("square");
+console.log(shape);
+
+//shape = ("circle");
+console.log(shape);
+
+var price = 20;
+console.log(price);
+
+price = 30;
+console.log(price);
 
 
 
