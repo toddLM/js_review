0ac1cf48d1a57e444aafd84d27782161ce7af45f
toddLM <65785960+toddLM@users.noreply.github.com>
2021-06-28 20:29:10 -0700

    working with Strings

--- assets/variables.js
+++ assets/variables.js
@@ -1,3 +1,6 @@
+/* Working with Variables
+console.log("working with Variables")
+
 let color = "red";
 console.log(color);
 
@@ -8,13 +11,71 @@ const shape = ("square");
 console.log(shape);
 
 //shape = ("circle");
-console.log(shape);
+//console.log(shape);
 
-var price = 20;
-console.log(price);
+//var price = 20;
+//console.log(price);
 
-price = 30;
-console.log(price);
+//price = 30;
+//console.log(price);
 
 
+// Working with Numbers
+console.log("working with Numbers")
 
+let x = 0;
+x = x + 15;
+console.log(x, "add 15");
+
+x -= 8;
+console.log(x, "subtract 8");
+
+let numAvg = (26 + 44 + 72 + 86 + 112) / 5
+console.log(numAvg, "average");
+
+
+let theNumber = 14;
+console.log(++theNumber);
+
+console.log(typeof x, "type");
+
+// Working with Strings
+
+console.log("working with Strings")
+let verb = 'drives';
+console.log(verb);
+
+let noun = 'car';
+console.log(noun);
+
+let adj = 'fast';
+console.log(adj);
+
+let coler = 'blue';
+console.log(color);
+
+let name = 'Todd';
+console.log(name);
+
+let madlib = 'book';
+console.log(madlib);
+
+
+console.log("This " + madlib, "is about how " + name, "\n"  + verb + " his " + adj, color, noun + "." + "\n"
++ "This is for Working with Strings section");
+
+madlibLiteral = `${name} always ${verb} his ${adj} ${color} ${noun}.`;
+console.log(madlibLiteral)
+*/
+
+// Conditionals
+
+let x = 2;
+let y = 4;
+if (x > y) {
+    console.log("Hello World");
+}
+if (x <= y) { 
+    console.log("Goodbye World");
+
+}
