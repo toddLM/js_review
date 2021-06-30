e6d33381ea50b82fde40f19b01902bc91c568138
toddLM <65785960+toddLM@users.noreply.github.com>
2021-06-29 21:16:02 -0700

    Day 2 Conditions Loops Functions

--- assets/variables.js
+++ assets/variables.js
@@ -66,16 +66,157 @@ console.log("This " + madlib, "is about how " + name, "\n"  + verb + " his " + a
 
 madlibLiteral = `${name} always ${verb} his ${adj} ${color} ${noun}.`;
 console.log(madlibLiteral)
-*/
+
 
 // Conditionals
 
 let x = 2;
 let y = 4;
 if (x > y) {
-    console.log("Hello World");
+    message = ("Hello World");
 }
-if (x <= y) { 
-    console.log("Goodbye World");
+else (x <= y) { 
+    messaghe = ("Goodbye World");
 
 }
+
+
+function sayHello(name){
+    return (name ? `Hello, ${name}!` : "Hello, person!")
+}
+console.log( sayHello("Class") )
+
+
+/*
+let soldOut = true;
+let onSale = true
+let message = "";
+
+soldOut = !soldOut
+
+//let thisMessage = (!soldOut && onSale) ? "I got tickets!" : "I did not get tickets";
+
+console.log(thisMessage);
+
+*/
+
+// Loops
+
+//for (let i = 0; i <= 100; i++) {
+   //console.log(i);
+/////} 
+
+//for (let i = 0; i <= 100; i+=2) {
+  // console.log(i);
+//} 
+
+//for (let i=100; i>=0; i--) {
+   //console.log(i);
+//} 
+
+/*
+
+
+//Functions
+
+// Number 1 - Functions
+
+function greeting() {
+    console.log('Hello World');
+}
+greeting();
+
+// Number 2 - Functions
+
+let countingCharacters = function (string) {
+    if (string.length <= 10) {
+        console.log(string.length);
+    } 
+    else {
+        console.log("The string: " + string + "is too long.")
+    }
+}
+
+countingCharacters("Today is the first day of the rest of your life, ");
+
+
+// Number 3 - Functions
+
+function difference (num1,num2) {
+    if (typeof(num1 || num2) !== 'number') {
+    }
+    else{
+        return Math.abs(num1 - num2);
+        //console.log(y-x);
+    }
+}
+
+console.log(difference(15,100));
+
+
+// Number 4 - Functions
+
+let advancedGreeting = function(hour){
+    if (hour < 12){
+        return "Good Morning";
+    }
+    else if (hour >= 12 && hour < 18){
+        return "Good afternoon";
+    }
+    else if (hour >= 18 && hour <= 24){
+        return "Good night";
+    }
+    else{
+        return "Invalid hour input";
+    }
+}
+
+console.log(advancedGreeting(8));
+*/
+//const values = ['a', 'b', 'c'];
+//values[1] = 'bbb';
+//console.log(values[1]);
+//console.log(values[1]);
+//console.log(values[2]);
+//console.log(values[3]);
+
+
+// Arrays - Number 5
+const carMakes = ["Honda", "Toyota", "Mazda"];
+carMakes.push("Mercedes");
+const first = carMakes.unshift("BMW");
+console.log(carMakes);
+console.log(carMakes[2]);
+
+
+// Arrays - Number 6
+const newCars = carMakes.slice(0,3); 
+console.log(newCars);
+
+
+// Arrays - Number 7
+const ericInfo = ["Eric", "Senior Instructor", "Florida"];
+const first2 = ericInfo.shift();
+console.log(first2);
+ericInfo[1] = "South Carolina";
+console.log(ericInfo);
+ericInfo.push("Schwartz");
+console.log(ericInfo);
+
+
+// Arrays - Number 8
+const fruits1 = ["apple", "pear", "strawberry", "grape"];
+const fruits2 = [
+  ["strawberry", "grape"],
+  ["rambutan", "durian"],
+  ["orange", "apple"]
+];
+const fruits3 = [
+  ["strawberry", ["pineapple"]],
+  ["grape", ["pear", ["apple"], "kiwi"]]
+];
+
+console.log(fruits1[0]);
+console.log(fruits2[:5]);
+console.log(fruits3[0]);
+
